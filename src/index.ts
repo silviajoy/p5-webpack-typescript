33095497f5e56079bc02025a72ed135670098c62
silviajoy <floriosg@gmail.com>
2019-03-12 13:28:06 +0100

    p5 setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import * as p5 from 'p5'
 
-let s = (sk) => {    
-    sk.setup = () =>{
+let sketch = (p) => {    
+    p.setup = () =>{
 
     }
 
-    sk.draw = () =>{
-        sk.ellipse(50, 50, 80, 80);
+    p.draw = () =>{
+        p.ellipse(50, 50, 80, 80);
     }
 }
 
-const P5 = new p5(s)
+const P5 = new p5(sketch)
